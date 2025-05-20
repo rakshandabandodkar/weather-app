92adba763e3fbfb912d0bf53281462d4b89102e9
rakshandabandodkar <rakshandabandodkar@gmail.com>
2025-05-20 16:33:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import WeatherApp from './components/WeatherApp.jsx'
+import './components/WeatherApp.css'
+
+function App() {
+
+  return(
+    <>
+    <WeatherApp/>
+        {/* <Header/>
+        <Footer/> */}
+    </>
+  );
+}
+
+export default App
